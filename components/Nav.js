3ec07993bffdee5329d849420e703409137650b8
Jay WInebrenner <jaywinebrenner@gmail.com>
2023-06-16 09:10:15 -0700

    tweak global button

--- components/Nav.js
+++ components/Nav.js
@@ -9,13 +9,13 @@ const Nav = ({englishShowingGlobal}) => {
     return (
         <div className="nav">
             <Link href="/listen">
-                <h2 style={{ textDecoration: router.pathname === "/listen" ? "underline" : "none" }}>{!englishShowingGlobal ? "LISTEN" : "HÖREN"}</h2>
+                <h2 style={{ textDecoration: router.pathname === "/listen" ? "underline" : "none" }}>{englishShowingGlobal ? "LISTEN" : "HÖREN"}</h2>
             </Link>
             <Link href="/">
-                <h2 style={{ textDecoration: router.pathname === "/" ? "underline" : "none" }}>{!englishShowingGlobal ? "HOME" : "HEIM"}</h2>
+                <h2 style={{ textDecoration: router.pathname === "/" ? "underline" : "none" }}>{englishShowingGlobal ? "HOME" : "HEIM"}</h2>
             </Link>
             <Link href="/lyrics">
-                <h2 style={{ textDecoration: router.pathname === "/lyrics" ? "underline" : "none" }}>{!englishShowingGlobal ? "LYRICS" : "WÖRTER"}</h2>
+                <h2 style={{ textDecoration: router.pathname === "/lyrics" ? "underline" : "none" }}>{englishShowingGlobal ? "LYRICS" : "WÖRTER"}</h2>
             </Link>
   
         </div>
--- pages/listen.js
+++ pages/listen.js
@@ -3,7 +3,7 @@ const Listen = ({englishShowingGlobal}) => {
         <div className="listen">
             {/* <h3>DIE TROMMELN DER JUGEND</h3>
             <h3>HARELQUIN DEFIBRILLATOR</h3> */}
-            <h3>{!englishShowingGlobal ? "STREAM THE MUSIC IN COMPLETE TOTALITY RIGHT HERE WITH THIS WEB AUDIO INTERFACE" : "STREAMEN SIE DAS ALBUM IN SEINER VOLLSTÄNDIGKEIT HIER"}</h3>
+            <h3>{englishShowingGlobal ? "STREAM THE MUSIC IN COMPLETE TOTALITY RIGHT HERE WITH THIS WEB AUDIO INTERFACE" : "STREAMEN SIE DAS ALBUM IN SEINER VOLLSTÄNDIGKEIT HIER"}</h3>
  
             <div className='audio-wrapper'>
                 <audio
@@ -15,7 +15,7 @@ const Listen = ({englishShowingGlobal}) => {
                 </audio>
                 <p>Die Trommeln Der Jugend - Harlequin Defibrillator</p>
             </div>
-            <h3>{!englishShowingGlobal ? "OR FIND THE MUSIC BY CLICKING THE FAVORITE CORPORATION ICON BELOW YOU" : "ODER SUCHEN SIE DIE MUSIK, INDEM SIE UNTEN AUF DAS LIEBLINGS-UNTERNEHMENSSYMBOL KLICKEN"}</h3>
+            <h3>{englishShowingGlobal ? "OR FIND THE MUSIC BY CLICKING THE FAVORITE CORPORATION ICON BELOW YOU" : "ODER SUCHEN SIE DIE MUSIK, INDEM SIE UNTEN AUF DAS LIEBLINGS-UNTERNEHMENSSYMBOL KLICKEN"}</h3>
             <div className="icon-wrapper">
                 <a target="_blank" rel="noreferrer" href="https://bandcamp.com" className="img-wrapper">
                     <img  src="/bc.png" />
